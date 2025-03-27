073c7970c397dbc9b3823c9b20d2257f1d9c6525
MaxBro <activer12@mail.ru>
2025-03-27 18:06:44 +0300

    feat: and one more api test

--- tests/test_api_incidents.py
+++ tests/test_api_incidents.py
@@ -46,6 +46,27 @@ async def test_post_wrong_incident(test_client: AsyncClient, test_db: AsyncSessi
     assert res.status_code == 404
 
 
+@pytest.mark.asyncio
+async def test_post_incident_wrong_app(test_client: AsyncClient, test_db: AsyncSession):
+    test_db.add(
+            App(name="Test App", url="http://test_app", dispatcher_code="123testcode")
+        )
+    await test_db.commit()
+    res = await test_client.post('incidents/post_incident', json={
+        'title': 'Test Incident',
+        'message': 'Test router correct post',
+        'level': 'info',
+        'logs': 'some errror\nsome log',
+        'app_name': 'Wrong App'
+    }, headers={
+        'Content-Type': 'application/json',
+        'dispatch': '123testcode',
+    })
+    assert res.status_code == 404
+    assert (await test_db.execute(
+        select(BannedIP).where(BannedIP.ip == '127.0.0.1')
+    )).scalar_one_or_none() is not None
+
 
 @pytest.mark.asyncio
 async def test_post_incident_no_dispatch(test_client: AsyncClient, test_db: AsyncSession):
