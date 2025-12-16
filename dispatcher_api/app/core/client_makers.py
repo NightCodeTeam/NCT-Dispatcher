5e65695906ca65f6e95e23f9df35440385007e57
Maxim Bodrov <activer12@mail.ru>
2025-12-16 12:19:47 +0300

    fix: nct-auth client

--- dispatcher_api/app/core/client_makers.py
+++ dispatcher_api/app/core/client_makers.py
@@ -9,13 +9,22 @@ class NCTAuth(HttpMakerAsync):
         )
 
     async def in_ban(self, user_ip: str) -> bool:
-        return (await self._make(f'/v1/bans/{user_ip}', 'GET')).json.get('ok', True)
+        res = await self._make(f'/v1/bans/{user_ip}', 'GET')
+        if res is None:
+            return True
+        return res.json.get('ok', True)
 
     async def ban(self, user_ip: str, reason: str | None = None) -> bool:
-        return await self._make(f'/v1/bans/{user_ip}', 'POST', params={'reason': reason}).json.get('ok', False)
+        res = await self._make(f'/v1/bans/{user_ip}', 'POST', params={'reason': reason})
+        if res is None:
+            return True
+        return res.json.get('ok', False)
 
     async def bans(self) -> list | tuple:
-        return (await self._make('/v1/bans', 'GET')).json.get('bans', [])
+        res = await self._make('/v1/bans', 'GET')
+        if res is None:
+            return tuple()
+        return res.json.get('bans', tuple())
 
 
 nct_auth = NCTAuth()
