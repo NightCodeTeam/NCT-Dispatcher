8ef22e99de659889b7455aea62212f2517d22db9
Maxim Bodrov <activer12@mail.ru>
2026-02-09 15:58:26 +0300

    fix: api response

--- dispatcher_front/src/api/main.jsx
+++ dispatcher_front/src/api/main.jsx
@@ -11,10 +11,10 @@ const api = axios.create({
 api.interceptors.response.use(
     (response) => response,
     (error) => {
-        if (error.response?.status === 401) {
+        if (error.response?.status === 401 || error.response?.status === 405) {
             window.location.href = '/auth/login';
         }
-        if (error.response?.status === 401) {
+        if (error.response?.status === 403) {
             window.location.href = '/auth/login';
         }
         return Promise.reject(error);
