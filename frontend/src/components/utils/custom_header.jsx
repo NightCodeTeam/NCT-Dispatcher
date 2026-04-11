d02e4f4fb789798fdd8fe90e51aca3bdc79728de
Maxim Bodrov <activer12@mail.ru>
2026-04-11 21:26:07 +0300

    feat: added mobile header

--- frontend/src/components/utils/custom_header.jsx
+++ frontend/src/components/utils/custom_header.jsx
@@ -20,7 +20,7 @@ function get_current_window(headers) {
 }
 
 
-const MobileHeaderView = ({headers, set_show}) => {
+const MobileHeaderView = ({headers, set_show, theme, handle_theme, user}) => {
     const navigate = useNavigate();
     const mobile_headers = parse_mobile_headers(headers);
 
@@ -49,20 +49,47 @@ const MobileHeaderView = ({headers, set_show}) => {
 
     return <div className='header_overlay-backdrop' onClick={() => handleOuterClick()}>
         <div className='header_overlay-content base_flex_column rounded_border' style={{
-            flexWrap: 'nowrap',
-
-            backgroundColor: 'var(--header-color)',
-            color: 'var(--header-text-color)',
-
-            height: '30vh',
             padding: '5px'
         }} onClick={(e) => handleInnerClick(e)}>
             {mobile_headers.map((header, i) => <div key={i} style={{
                 backgroundColor: is_current_window(header.path) ? 'var(--header-current-color)': 'inherit',
                 color: is_current_window(header.path) ? 'var(--header-current-text-color)': 'var(--header-text-color)',
-                textDecoration: 'none',
                 ...header.m_style,
             }} onClick={() => nav(header.path)} className='header_a_c'>{header.label}</div>)}
+            <div className='base_flex_row' style={{
+                width: '100%',
+                flexWrap: 'nowrap',
+                justifyContent: 'space-between',
+                maxHeight:'10vh',
+                backgroundColor: 'inherit'
+            }}>
+                {theme === 'dark' ? (
+                    <div className='header_a_theme base_flex_column' style={{height:'10vh', marginLeft: '10px'}} onClick={() => handle_theme('light')}>
+                        <img src={light} alt="светлая тема" className='header_a_theme_icon'/>
+                    </div>
+                ) : (
+                    <div className='header_a_theme base_flex_column' style={{height:'10vh', marginLeft: '10px'}} onClick={() => handle_theme('dark')}>
+                        <img src={dark} alt="темная тема" className='header_a_theme_icon'/>
+                    </div>
+                )}
+                {user.name === null || user.name === undefined ? (
+                    <div onClick={() => nav('/auth/login')} style={{
+                        height: '10vh',
+                        padding: '0 15px',
+                        borderRadius: '10px',
+                        backgroundColor: is_current_window('/auth/login') ? 'var(--header-current-color)': 'inherit',
+                        color: is_current_window('/auth/login') ? 'var(--header-current-text-color)': 'var(--header-text-color)',
+                    }} className='header_a'>Вход</div>
+                ):(
+                    <div onClick={() => nav('/auth/user')} style={{
+                        height: '10vh',
+                        padding: '0 15px',
+                        borderRadius: '10px',
+                        backgroundColor: is_current_window('/auth/user') ? 'var(--header-current-color)': 'inherit',
+                        color: is_current_window('/auth/user') ? 'var(--header-current-text-color)': 'var(--header-text-color)',
+                    }} className='header_a'>Профиль</div>
+                )}
+            </div>
         </div>
     </div>
 }
@@ -78,41 +105,6 @@ const CustomHeader = ({logo, headers}) => {
     const handle_theme = (new_theme) => {
         set_theme(new_theme);
     }
-
-    const adt_headers = [
-        {
-            path: undefined,
-            label: theme === 'dark' ? <img src={light} alt="светлая тема" style={{
-                width: '25px',
-                height: '25px',
-                backgroundColor: 'var(--header-color)',
-                padding: '1px',
-            }} onClick={() => handle_theme('light')}/>: <img src={dark} alt="темная тема" style={{
-                width: '25px',
-                height: '25px',
-                backgroundColor: 'var(--header-color)',
-                padding: '1px',
-            }} onClick={() => handle_theme('dark')}/>,
-            d_style: {
-                marginLeft: 'auto',
-                marginRight: '10px',
-            }
-        },
-        ...user.name === null || user.name === undefined ? [{
-            path: '/auth/login',
-            label: 'Вход',
-            d_style: {
-                marginRight: '10px',
-            }
-        }]:[{
-            path: '/auth/logout',
-            label: `Выход (${user.name})`,
-            d_style: {
-                marginRight: '10px',
-            }
-        }],
-    ]
-    const full_headers = [...headers, ...adt_headers]
 
     useEffect(() => {
 
@@ -130,7 +122,13 @@ const CustomHeader = ({logo, headers}) => {
                     backgroundColor: 'var(--header-color)',
                     boxShadow: 'none',
                 }} onClick={() => set_mobile_show_view(!mobile_show_view)}>···</button>
-                {mobile_show_view && <MobileHeaderView headers={headers} set_show={set_mobile_show_view}x/>}
+                {mobile_show_view && <MobileHeaderView
+                    headers={headers}
+                    set_show={set_mobile_show_view}
+                    theme={theme}
+                    handle_theme={handle_theme}
+                    user={user}
+                />}
             </div>
             ):(
             <div className='base_flex_row header_container'>
