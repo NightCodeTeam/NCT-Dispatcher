345de2ee48d7024eb3dc3b748122d727bd77f65e
Maxim Bodrov <activer12@mail.ru>
2026-04-19 13:15:42 +0300

    fix: auth reload

--- frontend/src/context/auth.jsx
+++ frontend/src/context/auth.jsx
@@ -14,11 +14,12 @@ export function AuthProvider({ children }) {
 
     const check_auth = async () => {
         try {
+            set_loading(true);
             const new_user = await auth_service.user()
-            set_user(prev => ({ ...prev, name: new_user.name }))
+            set_user({name: new_user.name })
         } catch (error) {
             if (error.status === 401) {
-                set_user({name: null});
+                set_user(prev => ({ ...prev, name: null }))
             }
         } finally {
             set_loading(false);
@@ -30,14 +31,17 @@ export function AuthProvider({ children }) {
     }, []);
 
     const login = async (name, password) => {
+        set_loading(true);
         const data = await auth_service.login(name, password);
-        set_user(data)
-        check_auth()
+        set_user({ name: data.name })
+        set_loading(false);
     };
 
     const logout = async () => {
+        set_loading(true);
         await auth_service.logout();
-        set_user(null);
+        set_user({ name: null })
+        set_loading(false);
     };
 
     if (loading) {
