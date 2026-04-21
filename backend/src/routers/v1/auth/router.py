492b21aac4df901192e53751481a32e866bfe513
Maxim Bodrov <activer12@mail.ru>
2026-04-21 10:18:26 +0300

    fix: auth route details

--- backend/src/routers/v1/auth/router.py
+++ backend/src/routers/v1/auth/router.py
@@ -12,7 +12,7 @@ from .models import User
 auth_router_v1 = APIRouter(prefix='/v1/auth', tags=['auth'])
 
 
-@auth_router_v1.post('/login', response_model=Ok)
+@auth_router_v1.post('/login', response_model=Ok | Detail)
 async def login(response: Response, user_data: UserLogin):
     """
     Вход пользователя в систему.
@@ -20,7 +20,7 @@ async def login(response: Response, user_data: UserLogin):
     return {'ok': await AuthHandler().login(user_data, response)}
 
 
-@auth_router_v1.post('/refresh', response_model=Ok)
+@auth_router_v1.post('/refresh', response_model=Ok | Detail)
 async def refresh(request: Request, response: Response):
     """
     Обновление токена доступа.
@@ -28,7 +28,7 @@ async def refresh(request: Request, response: Response):
     return {'ok': True}
 
 
-@auth_router_v1.post('/logout', response_model=Ok)
+@auth_router_v1.post('/logout', response_model=Ok | Detail)
 async def logout(response: Response, user: UserDep):
     """
     Выход пользователя из системы.
@@ -36,7 +36,7 @@ async def logout(response: Response, user: UserDep):
     return {'ok': await AuthHandler().logout(user=user, response=response)}
 
 
-@auth_router_v1.post('/register', response_model=Ok)
+@auth_router_v1.post('/register', response_model=Ok | Detail)
 async def register(request: Request, user_data: UserRegister):
     """
     Регистрация нового пользователя.
